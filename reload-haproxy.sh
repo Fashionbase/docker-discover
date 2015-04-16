a9208d251f8a4bc47bd2e4a212f1695957cb9bac
Roch Delsalle <rdelsalle@gmail.com>
2015-04-16 15:36:32 +0200

    True Zero Downtime HAProxy Reloads #2

--- reload-haproxy.sh
+++ reload-haproxy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+sudo nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --buffer &> /dev/null
 /usr/local/sbin/haproxy -f /etc/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)
+sudo nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug--release-indefinite &> /dev/null
+sleep 0.100
